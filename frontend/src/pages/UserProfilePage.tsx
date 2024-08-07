c9e7cf11b15c01fa318f61f7e86bf87b54e9d241
swaroop-2007 <73997421+swaroop-2007@users.noreply.github.com>
2024-08-07 03:37:28 -0500

    Update: Made changes to userprofile form to see the details on the user profile

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -8,7 +8,13 @@ const UserProfilePage = () => {
     if(isGetLoading) {
         return <span>Loading...</span>;
     }
-    return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading}/>;
+    if(!currentUser) {
+        return <span>Unable to Load User Profile!</span>;
+    }
+    return <UserProfileForm 
+    currentUser = {currentUser}
+    onSave={updateUser} 
+    isLoading={isUpdateLoading}/>;
 };
 
 export default UserProfilePage;
